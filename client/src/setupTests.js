8a708058d8dfb793fdfed66ca3480ca3dd2765fb
jannkasper <jann.kasper@gmail.com>
2021-05-11 07:33:53 +0200

    client | test | setup

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,8 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+// this adds jest-dom's custom assertions
+import '@testing-library/jest-dom/extend-expect';
+import { enableFetchMocks } from 'jest-fetch-mock';
+
+configure({ adapter: new Adapter() });
+enableFetchMocks();
